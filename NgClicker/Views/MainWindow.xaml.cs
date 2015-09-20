88bdd44edc2f85ba0759231e58d5b1964c99fc79
wojciech.luczkow <w.luczkow@gmail.com>
2015-09-20 22:38:16 +0200

    Moved MainWindow to new Views folder

---
+++ NgClicker/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace NgClicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
